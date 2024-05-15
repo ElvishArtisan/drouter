bd10826a5c16b1e1cab13ebe5280e613b6db68b7
Fred Gleason <fredg@paravelsystems.com>
2024-05-15 14:02:06 -0400

    2024-05-15 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a bug in dmap(1) that caused map output to specify a 	'RouterNumber=' value of '0' when no '--router-number' switch 	was given.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- src/dmap/dmap.cpp
+++ src/dmap/dmap.cpp
@@ -40,7 +40,7 @@ MainObject::MainObject(QObject *parent)
 
   map_output_map="";
   map_save_names=false;
-  map_router_number=0;
+  map_router_number=1;
   map_router_name="Livewire";
   map_router_type=EndPointMap::AudioRouter;
   map_verbose=false;
