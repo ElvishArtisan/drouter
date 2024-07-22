92d8ef44e2ea3f8509f4590ca0309d9097980150
Fred Gleason <fredg@paravelsystems.com>
2024-07-22 16:36:54 -0400

    2024-07-22 Fred Gleason <fredg@paravelsystems.com> 	* Removed superfluous rivendell elements from 'configure.ac'. 	* Fixed a bug in 'configure.ac' that failed to correctly identify 	Linux Mint.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- debian/python_install.sh
+++ debian/python_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if test -d $1/dist-packages ; then
-    mv $1/dist-packages/* debian/drouter-python/usr/lib/python3/dist-packages/
-else
-    mv $1/site-packages/* debian/drouter-python/usr/lib/python3/dist-packages/
-fi
+#if test -d $1/dist-packages ; then
+#    mv $1/dist-packages/* debian/drouter-python/usr/lib/python3/dist-packages/
+#else
+#    mv $1/site-packages/* debian/drouter-python/usr/lib/python3/dist-packages/
+#fi
