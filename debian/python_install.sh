b403d86d8f483af29ad43cdd2066cf87a04f272e
Fred Gleason <fredg@paravelsystems.com>
2024-07-22 15:46:32 -0400

    2024-07-22 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a bug that broke the 'make deb' target on Raspberry Pi OS.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ debian/python_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if test -d $1/dist-packages ; then
+    mv $1/dist-packages/* debian/drouter-python/usr/lib/python3/dist-packages/
+else
+    mv $1/site-packages/* debian/drouter-python/usr/lib/python3/dist-packages/
+fi
