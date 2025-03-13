9f9a94045dde79979ad993bf78bdb2d6586313a0
Fred Gleason <fredg@paravelsystems.com>
2025-03-13 13:44:02 -0400

    2025-03-13 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a bug in buttonpanel(1) that caused sound files in 	'/usr/share/drouter/sounds/' to fail to be included in the sound 	file path.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- src/buttonpanel/buttonpanel.cpp
+++ src/buttonpanel/buttonpanel.cpp
@@ -162,7 +162,7 @@ MainWidget::MainWidget(QWidget *parent)
   //
   QStringList paths;
   paths.push_back("/etc/drouter/sounds");
-  paths.push_back(QString(PATH_DATA)+"/drouter/sounds");
+  paths.push_back(QString(PATH_DATA)+"/sounds");
   if(!panel_sound_alert_file.isEmpty()) {
     bool found=false;
     for(int i=0;i<paths.size();i++) {
